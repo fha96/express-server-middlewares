fcfcf2565d66fbb1a9a8177a040db78350759919
fha96 <fahadalrimawe96@hotmail.com>
2022-09-06 19:44:09 +0300

    add testing unit for middle ware

--- __test__/validate.test.js
+++ __test__/validate.test.js
@@ -4,4 +4,21 @@
 const validate = require('../middlewares/validate-number.js');
 
 
-describe('testing the first middle ware')
+describe('testing the first middle ware', () => {
+
+    let spy;
+    //before each testing units
+    beforeEach(()=>{
+        spy = jest.spyOn(console,'log').mockImplementation();
+    });
+    let req={"query":
+    {
+        "num":"5"
+    }};
+    let res={};
+    let next=jest.fn();
+    it ('testing validateNumber',  () => {
+        validate(req, res, next);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+})
--- middlewares/validate-number.js
+++ middlewares/validate-number.js
@@ -3,13 +3,12 @@
 
 module.exports =  (req, res, next ) => {
      let num = req.query.num;
-    const rgx = /\b[0-9][0-9]*\b/g;
-    let result=num.match(rgx);
-    console.log(result);
-
+     const rgx = /\b[0-9][0-9]*\b/g;
+     let result= num.match(rgx);
+     console.log(result);
+     
     if(result) {
         let newNumber = parseInt(result[0]);
-        console.log(newNumber);
         req.query.num=newNumber*newNumber;
         next();
     } else {
